c34d42dca3cf3d692562e060d21512a7f83db320
Ostap Danylovych <music.danos@gmail.com>
2021-08-27 23:01:29 +0300

    reworked AssetId as struct

--- Sources/Avalanche/Transactions/TransferableOutput.swift
+++ Sources/Avalanche/Transactions/TransferableOutput.swift
@@ -7,11 +7,30 @@
 
 import Foundation
 
+public struct AssetId {
+    public static let size = 32
+    
+    public let data: Data
+    
+    public init?(data: Data) {
+        guard data.count == Self.size else {
+            return nil
+        }
+        self.data = data
+    }
+}
+
+extension AssetId: AvalancheEncodable {
+    public func encode(in encoder: AvalancheEncoder) throws {
+        try encoder.encode(data, size: Self.size)
+    }
+}
+
 public struct TransferableOutput {
-    public let assetId: Data
+    public let assetId: AssetId
     public let output: Output
     
-    public init(assetId: Data, output: Output) {
+    public init(assetId: AssetId, output: Output) {
         self.assetId = assetId
         self.output = output
     }
@@ -19,6 +38,6 @@ public struct TransferableOutput {
 
 extension TransferableOutput: AvalancheEncodable {
     public func encode(in encoder: AvalancheEncoder) throws {
-        try encoder.encode(assetId, size: 32).encode(output)
+        try encoder.encode(assetId).encode(output)
     }
 }
