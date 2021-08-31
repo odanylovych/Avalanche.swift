2eebc0d5e9b52b851f2f6cf341d32a370400a120
Yehor Popovych <popovych.yegor@gmail.com>
2021-08-31 21:33:15 +0300

    b58 code cleanup

--- Sources/Avalanche/Algos/Base58Algos.swift
+++ Sources/Avalanche/Algos/Base58Algos.swift
@@ -13,10 +13,7 @@ public struct Base58Algos {
     public static let checksumLength = 4
     public static let b58Alphabet = [UInt8]("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".utf8)
     
-    public func b58(
-        data: Data,
-        alphabet: [UInt8] = Self.b58Alphabet
-    ) -> String {
+    public func b58(data: Data, alphabet: [UInt8] = Self.b58Alphabet) -> String {
         var x = BigUInt(data)
         let radix = BigUInt(alphabet.count)
 
@@ -35,10 +32,7 @@ public struct Base58Algos {
         return String(bytes: answer, encoding: .utf8)!
     }
 
-    public func from(
-        b58: String,
-        alphabet: [UInt8] = Self.b58Alphabet
-    ) -> Data? {
+    public func from(b58: String, alphabet: [UInt8] = Self.b58Alphabet) -> Data? {
         var answer = BigUInt(0)
         var j = BigUInt(1)
         let radix = BigUInt(alphabet.count)
@@ -57,24 +51,22 @@ public struct Base58Algos {
         return byteString.prefix(while: { i in i == alphabet[0]}).map{ _ in 0 } + bytes
     }
     
-    public func from(cb58: String) -> Data? {
-        guard let b58data = from(b58: cb58) else {
+    public func from(cb58: String, alphabet: [UInt8] = Self.b58Alphabet) -> Data? {
+        guard let b58data = from(b58: cb58, alphabet: alphabet) else {
             return nil
         }
-        print("DATA", b58data)
         let prefix = b58data.prefix(b58data.count - Self.checksumLength)
         let checksum = b58data.suffix(Self.checksumLength)
-        print("CS", checksum)
-        let calculated = SHA2.hash(type: .sha256, bytes: Array(prefix))
-        print("CALC", calculated.suffix(Self.checksumLength))
+        let calculated = SHA2.hash(type: .sha256, data: Data(prefix))
         guard Data(calculated.suffix(Self.checksumLength)) == checksum else {
             return nil
         }
         return Data(prefix)
     }
     
-    public func cb58(data: Data) -> String {
+    public func cb58(data: Data, alphabet: [UInt8] = Self.b58Alphabet) -> String {
         let checksum = SHA2.hash(type: .sha256, data: data)
-        return b58(data: data + checksum.suffix(Self.checksumLength))
+        return b58(data: data + checksum.suffix(Self.checksumLength),
+                   alphabet: alphabet)
     }
 }
