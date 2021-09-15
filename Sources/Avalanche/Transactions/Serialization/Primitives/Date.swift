ffb6ca34e3f283a4366c8e7c3f68e905d09f3869
Ostap Danylovych <music.danos@gmail.com>
2021-09-15 23:04:11 +0300

    added Date decoder

--- Sources/Avalanche/Transactions/Serialization/Primitives/Date.swift
+++ Sources/Avalanche/Transactions/Serialization/Primitives/Date.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-extension Date: AvalancheEncodable {
+extension Date: AvalancheCodable {
+    public init(from decoder: AvalancheDecoder) throws {
+        self.init(timeIntervalSince1970: TimeInterval(try UInt64(from: decoder)))
+    }
+    
     public func encode(in encoder: AvalancheEncoder) throws {
         try encoder.encode(UInt64(timeIntervalSince1970))
     }
