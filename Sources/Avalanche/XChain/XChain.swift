b096262c2bf61c55aa78df0edab254566ec42bb7
Ostap Danylovych <music.danos@gmail.com>
2021-10-08 16:23:27 +0300

    added avm.createFixedCapAsset api method

--- Sources/Avalanche/XChain/XChain.swift
+++ Sources/Avalanche/XChain/XChain.swift
@@ -9,6 +9,7 @@ import Foundation
 import BigInt
 #if !COCOAPODS
 import RPC
+import Serializable
 #endif
 
 public class AvalancheXChainApiInfo: AvalancheBaseVMApiInfo {
@@ -52,7 +53,66 @@ public class AvalancheXChainApi: AvalancheApi {
         
         self.service = JsonRpc(.http(url: avalanche.url(path: info.apiPath), session: settings.session, headers: settings.headers), queue: settings.queue, encoder: settings.encoder, decoder: settings.decoder)
         self.vmService = JsonRpc(.http(url: avalanche.url(path: info.vmApiPath), session: settings.session, headers: settings.headers), queue: settings.queue, encoder: settings.encoder, decoder: settings.decoder)
-        
+    }
+    
+    public struct InitialHolder: Encodable {
+        public let address: String
+        public let amount: UInt64
+    }
+    
+    public struct CreateFixedCapAssetParams: Encodable {
+        public let name: String
+        public let symbol: String
+        public let denomination: UInt32?
+        public let initialHolders: [InitialHolder]
+        public let from: [String]
+        public let changeAddr: String?
+        public let username: String
+        public let password: String
+    }
+    
+    public struct CreateFixedCapAssetResponse: Decodable {
+        public let assetID: String
+        public let changeAddr: String
+    }
+    
+    public func createFixedCapAsset(
+        name: String,
+        symbol: String,
+        denomination: UInt32? = nil,
+        initialHolders: [(address: Address, amount: UInt64)],
+        from: [String],
+        change: Address? = nil,
+        credentials: AvalancheVmApiCredentials,
+        _ cb: @escaping ApiCallback<(assetID: AssetID, change: Address)>
+    ) {
+        switch credentials {
+        case .password(let username, let password):
+            let params = CreateFixedCapAssetParams(
+                name: name,
+                symbol: symbol,
+                denomination: denomination,
+                initialHolders: initialHolders.map {
+                    InitialHolder(address: $0.address.bech, amount: $0.amount)
+                },
+                from: from,
+                changeAddr: change?.bech,
+                username: username,
+                password: password
+            )
+            service.call(
+                method: "avm.createFixedCapAsset",
+                params: params,
+                CreateFixedCapAssetResponse.self,
+                SerializableValue.self
+            ) { res in
+                cb(res
+                    .mapError(AvalancheApiError.init)
+                    .map { (AssetID(cb58: $0.assetID)!, try! Address(bech: $0.changeAddr)) })
+            }
+        case .account:
+            fatalError("Not implemented")
+        }
     }
 }
 
