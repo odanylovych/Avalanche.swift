bac1ed7b1de1ea57c5273ab2dd24da8ed10b1fab
Ostap Danylovych <19220558+odanylovych@users.noreply.github.com>
2021-12-16 05:22:20 +0200

    added test for platform.addDelegator

--- Tests/AvalancheTests/PChainTests.swift
+++ Tests/AvalancheTests/PChainTests.swift
@@ -37,11 +37,11 @@ final class PChainTests: XCTestCase {
         )
         idIndex = 0
         addressIndex = 0
-        avaxAssetID = newAssetID()
+        avaxAssetID = newID(type: AssetID.self)
         testFromAddress = newAddress()
         testChangeAddress = newAddress().address
-        testTransactionID = newTransactionID()
-        let utxoTransactionID = newTransactionID()
+        testTransactionID = newID(type: TransactionID.self)
+        let utxoTransactionID = newID(type: TransactionID.self)
         utxoForInput = UTXO(
             transactionID: utxoTransactionID,
             utxoIndex: 0,
@@ -113,16 +113,10 @@ final class PChainTests: XCTestCase {
         return connectionProvider
     }
     
-    private func newAssetID() -> AssetID {
-        let assetID = AssetID(data: Data(repeating: idIndex, count: AssetID.size))!
+    private func newID<T: ID>(type: T.Type) -> T {
+        let id = T(data: Data(repeating: idIndex, count: T.size))!
         idIndex += 1
-        return assetID
-    }
-    
-    private func newTransactionID() -> TransactionID {
-        let transactionID = TransactionID(data: Data(repeating: idIndex, count: TransactionID.size))!
-        idIndex += 1
-        return transactionID
+        return id
     }
     
     private func newAddress<API: AvalancheVMApi>(api: API) -> ExtendedAddress {
@@ -138,5 +132,143 @@ final class PChainTests: XCTestCase {
     
     private func newAddress() -> ExtendedAddress {
         newAddress(api: api)
+    }
+    
+    func testAddDelegator() throws {
+        let success = expectation(description: "success")
+        let nodeID = newID(type: NodeID.self)
+        let startTime = Date()
+        let endTime = startTime + 1000
+        let stakeAmount: UInt64 = 50_000_000
+        let reward = newAddress().address
+        let fromAddress = testFromAddress.address
+        let fromAddressPath = testFromAddress.path
+        let toAddress = newAddress().address
+        let memo = "memo".data(using: .utf8)!
+        let utxo = UTXO(
+            transactionID: newID(type: TransactionID.self),
+            utxoIndex: 0,
+            assetID: avaxAssetID,
+            output: try StakeableLockedOutput(
+                locktime: Date() + 100,
+                transferableOutput: TransferableOutput(
+                    assetID: newID(type: AssetID.self),
+                    output: SECP256K1TransferOutput(
+                        amount: 110_000_000,
+                        locktime: Date(timeIntervalSince1970: 0),
+                        threshold: 1,
+                        addresses: [testFromAddress.address]
+                    )
+                )
+            )
+        )
+        testUtxos.append(utxo)
+        let lockedOutput = utxo.output as! StakeableLockedOutput
+        let lockedOutputOutput = lockedOutput.transferableOutput.output
+        let lockedOutputAmount = (lockedOutputOutput as! SECP256K1TransferOutput).amount
+        let inputs = [
+            TransferableInput(
+                transactionID: utxo.transactionID,
+                utxoIndex: utxo.utxoIndex,
+                assetID: utxo.assetID,
+                input: StakeableLockedInput(
+                    locktime: lockedOutput.locktime,
+                    transferableInput: TransferableInput(
+                        transactionID: utxo.transactionID,
+                        utxoIndex: utxo.utxoIndex,
+                        assetID: utxo.assetID,
+                        input: try SECP256K1TransferInput(
+                            amount: lockedOutputAmount,
+                            addressIndices: lockedOutput.getAddressIndices(for: [fromAddress])
+                        )
+                    )
+                )
+            )
+        ]
+        let outputs = [
+            TransferableOutput(
+                assetID: avaxAssetID,
+                output: try StakeableLockedOutput(
+                    locktime: lockedOutput.locktime,
+                    transferableOutput: TransferableOutput(
+                        assetID: avaxAssetID,
+                        output: type(of: lockedOutputOutput).init(
+                            amount: lockedOutputAmount - stakeAmount,
+                            locktime: lockedOutputOutput.locktime,
+                            threshold: lockedOutputOutput.threshold,
+                            addresses: lockedOutputOutput.addresses
+                        )
+                    )
+                )
+            )
+        ]
+        let stakeOutputs = [
+            TransferableOutput(
+                assetID: avaxAssetID,
+                output: try StakeableLockedOutput(
+                    locktime: lockedOutput.locktime,
+                    transferableOutput: TransferableOutput(
+                        assetID: avaxAssetID,
+                        output: type(of: lockedOutputOutput).init(
+                            amount: stakeAmount,
+                            locktime: lockedOutputOutput.locktime,
+                            threshold: lockedOutputOutput.threshold,
+                            addresses: lockedOutputOutput.addresses
+                        )
+                    )
+                )
+            )
+        ]
+        let signatureProvider = SignatureProviderMock()
+        signatureProvider.signTransactionMock = { transaction, cb in
+            let extended = transaction as! ExtendedAvalancheTransaction
+            XCTAssertEqual(extended.pathes, [fromAddress: fromAddressPath])
+            XCTAssert(extended.utxoAddresses.first!.0 == SECP256K1Credential.self)
+            XCTAssertEqual(extended.utxoAddresses.first!.1, [fromAddress])
+            let transaction = extended.transaction as! AddDelegatorTransaction
+            let testTransaction = try! AddDelegatorTransaction(
+                networkID: self.api.networkID,
+                blockchainID: self.api.info.blockchainID,
+                outputs: outputs,
+                inputs: inputs,
+                memo: memo,
+                validator: Validator(
+                    nodeID: nodeID,
+                    startTime: startTime,
+                    endTime: endTime,
+                    weight: stakeAmount
+                ),
+                stake: Stake(lockedOutputs: stakeOutputs),
+                rewardsOwner: SECP256K1OutputOwners(
+                    locktime: Date(timeIntervalSince1970: 0),
+                    threshold: 1,
+                    addresses: [reward]
+                )
+            )
+            XCTAssertEqual(transaction, testTransaction)
+            cb(.success(SignedAvalancheTransaction(
+                unsignedTransaction: transaction,
+                credentials: []
+            )))
+        }
+        avalanche.signatureProvider = signatureProvider
+        api.addDelegator(
+            nodeID: nodeID,
+            startTime: startTime,
+            endTime: endTime,
+            stakeAmount: stakeAmount,
+            reward: reward,
+            from: [fromAddress],
+            to: [toAddress],
+            change: testChangeAddress,
+            memo: memo,
+            credentials: .account(testAccount)
+        ) { res in
+            let (transactionID, changeAddress) = try! res.get()
+            XCTAssertEqual(transactionID, self.testTransactionID)
+            XCTAssertEqual(changeAddress, self.testChangeAddress)
+            success.fulfill()
+        }
+        wait(for: [success], timeout: 10)
     }
 }
