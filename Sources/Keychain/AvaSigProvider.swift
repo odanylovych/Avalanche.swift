770ff2a0f453562be1f2614b7a1b7d3e614a5dd3
Ostap Danylovych <19220558+odanylovych@users.noreply.github.com>
2022-02-19 03:49:05 +0200

    fix derive for cchain address path

--- Sources/Keychain/AvaSigProvider.swift
+++ Sources/Keychain/AvaSigProvider.swift
@@ -37,7 +37,7 @@ private extension AvalancheBip44Keychain {
         var keypairs: [(T.Addr.Extended, KeyPair)] = []
         keypairs.reserveCapacity(addresses.count)
         for address in addresses {
-            switch deriveKeyPair(for: address.path, isEthereum: isEthereum) {
+            switch deriveKeyPair(for: address.path) {
             case .failure(let err):
                 cb(.failure(err))
                 return
@@ -65,10 +65,8 @@ private extension AvalancheBip44Keychain {
         }
     }
     
-    private func deriveKeyPair(
-        for path: Bip32Path, isEthereum: Bool
-    ) -> AvalancheSignatureProviderResult<KeyPair> {
-        if isEthereum {
+    private func deriveKeyPair(for path: Bip32Path) -> AvalancheSignatureProviderResult<KeyPair> {
+        if path.isValidEthereumAccount {
             guard let kp = _ethCache[path.accountIndex!] else {
                 return .failure(.accountNotFound(account: path.account!))
             }
@@ -122,9 +120,7 @@ extension AvalancheBip44Keychain: AvalancheSignatureProvider {
         _ cb: @escaping (AvalancheSignatureProviderResult<Signature>) -> Void) {
         DispatchQueue.global().async {
             let isEthereum = address is EthAccount
-            switch self.deriveKeyPair(for: address.path,
-                                      isEthereum: isEthereum
-            ) {
+            switch self.deriveKeyPair(for: address.path) {
             case .failure(let err):
                 cb(.failure(err))
             case .success(let kp):
